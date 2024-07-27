ccd5076bfde5d782921b1779b6dc8ec04a4a38c3
Fscarmon <156925521+Fscarmon@users.noreply.github.com>
2024-07-27 18:03:28 +0800

    Add files via upload

--- init.sh
+++ init.sh
@@ -335,7 +335,7 @@ get_country_code() {
         fi
     done
 
-    echo "     国家:    $country_code"
+    echo "country:    [$country_code"]
 }
 get_country_code
 XIEYI=${XIEYI:-'vl'}
@@ -343,8 +343,8 @@ CF_IP=${CF_IP:-'ip.sb'}
 SUB_NAME=${SUB_NAME:-'docker'}
 up_url="${XIEYI}ess://${UUID}@${CF_IP}:443?path=%2F${XIEYI}s%3Fed%3D2048&security=tls&encryption=none&host=${ARGO_DOMAIN}&type=ws&sni=${ARGO_DOMAIN}#${country_code}-${SUB_NAME}"
 encoded_url=$(echo -n $up_url | base64 -w 0)
-echo "=====  <节点信息>  =====  "
-echo "=============================="
+echo "  "
+echo "==========<Url code>==========="
 echo "$encoded_url"
 echo "=============================="
 fi
