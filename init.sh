40978e079d9b894edbb091e82bfebe66fb662b1c
Fscarmon <156925521+Fscarmon@users.noreply.github.com>
2024-08-22 21:45:54 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -164,7 +164,7 @@ site:
 EOF
 
   # 下载包含本地数据的 sqlite.db 文件，生成18位随机字符串用于本地 Token
-  wget -P ${WORK_DIR}/data/sqlite.db ${GH_PROXY}https://github.com/Fscarmon/nodejs_nezha_server/raw/main/sqlite.db
+  wget -P ${WORK_DIR}/data/ ${GH_PROXY}https://github.com/Fscarmon/nodejs_nezha_server/raw/main/sqlite.db
   [ -z "$NO_SUIJI" ] && LOCAL_TOKEN=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 18)
   [ -n "$NO_SUIJI" ] && LOCAL_TOKEN="$NO_SUIJI"
   sqlite3 ${WORK_DIR}/data/sqlite.db "update servers set secret='${LOCAL_TOKEN}' where created_at='2023-04-23 13:02:00.770756566+08:00'"
